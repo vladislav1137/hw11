3754f8b40b49dc8d6c3eff5079f48cf9a1179c88
vladislav1137 <mr.vladislav220@yandex.ru>
2023-03-09 02:12:06 +0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+
+
 public class Main {
 
-    public static void calculateYear(int year) {
+    public static void calculateLeapYear(int year) {
         System.out.println("Задача 1");
         if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
@@ -8,8 +11,40 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
-    public static void main(String[] args) {
-       calculateYear(2021);
 
+    public static void device(int clientOS, int clientDeviceYear) {
+        System.out.println("Задача 2");
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS != 0) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
     }
-}
+
+    public static void deliveryDays(int deliveryDistance) {
+        System.out.println("Задача 3");
+        if (deliveryDistance < 20) {
+            System.out.println("на доставку потребуется 1 день");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("на доставку потребуется 2 дня");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("на доставку потребуется 3 дня");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Доставка свыше 100 км не осуществляется");
+        }
+    }
+        public static void main (String[]args) {
+            calculateLeapYear(2021);
+            device(1, 2022);
+            deliveryDays(95);
+        }
+    }
