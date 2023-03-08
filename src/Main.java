9b762b394c41674a9f12ab7bdf37f91885e1c40b
vladislav1137 <mr.vladislav220@yandex.ru>
2023-03-09 00:57:28 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
 
-    public static int calculateYear() {
-        return 2021;
-    }
-    public static void main(String[] args) {
-        int year = calculateYear();
-        if ((year % 4 == 0) && year % 100 != 0) {
-            System.out.println(year + " год является високосным");
-        } else if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+    public static void calculateYear(int year) {
+        System.out.println("Задача 1");
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
         }
     }
-}
+    public static void main(String[] args) {
+       calculateYear(2021);
 
+    }
+}
